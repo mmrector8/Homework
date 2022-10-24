d2d0b0f94627673f3feb2dd0658c9727c3b27d39
Morgan Rector <mmrector@yahoo.com>
2022-10-23 20:00:14 -0700

    BFS

--- w4D1hmrk/graph_bfs.rb
+++ w4D1hmrk/graph_bfs.rb
@@ -1,39 +1,31 @@
 class GraphNode
     attr_reader :value
     attr_accessor :neighbors
+
     def initialize(value)
         @value = value
         @neighbors = []
     end
 
-    def bfs(starting_node, target_value)
+ def bfs(starting_node, target_value)
         #define a new set to put already checked values in
         visited = Set.new
         visited.add(starting_node)
         to_check = [starting_node]
         #loop through set starting at the starting node
         while to_check.length > 0
-            if to_check.value.shift == target_value
-                return to_check.shift
+            first_val = to_check.shift
+
+            if first_val.value == target_value
+                return first_val
             else
-                to_check.neighbors.each do |neighbor|
-                    return nil if visited.include?(neighbor)
+                first_val.neighbors.each do |neighbor|
+                    if !visited.include?(neighbor)
                     to_check << neighbor
+                    end
                 end
             end
         end
     end
 end
 
-a = GraphNode.new('a')
-b = GraphNode.new('b')
-c = GraphNode.new('c')
-d = GraphNode.new('d')
-e = GraphNode.new('e')
-f = GraphNode.new('f')
-a.neighbors = [b, c, e]
-c.neighbors = [b, d]
-e.neighbors = [a]
-f.neighbors = [e]
-
-p bfs(a, "b")
