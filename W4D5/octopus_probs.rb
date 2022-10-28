1a496446861540c46eb7876f52a9db9b6c871aa9
Morgan Rector <mmrector@yahoo.com>
2022-10-27 19:53:53 -0700

    Method skeletons added

---
+++ W4D5/octopus_probs.rb
@@ -0,0 +1,21 @@
+octo_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 
+ 'fiiiissshhhhhh']
+
+def sluggish_octo(arr)
+
+end
+
+def dominant_octo
+end
+
+def clever_octo
+end
+
+def dancing_octo
+end
+
+def slow_dance
+end
+
+def constant_dance
+end
