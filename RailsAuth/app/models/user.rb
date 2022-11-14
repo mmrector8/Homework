c7a9698976920a5c5c945c37740b9a38e7f1d2e1
Morgan Rector <mmrector@yahoo.com>
2022-11-13 18:13:00 -0800

    All encryption completed, final tests pending

--- RailsAuth/app/models/user.rb
+++ RailsAuth/app/models/user.rb
@@ -1,9 +1,42 @@
 class User < ApplicationRecord
+    attr_reader :password
     before_validation :ensure_session_token
     validates :username, :session_token,  presence: true
     validates :password_digest, presence: {message: "Password can't be blank"}
     validates :password_digest, length: {minimum: 6, allow_nil: true}
 
 
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil if user.nil?
+        user.is_password?(password) ? user :nil
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+    def generate_unique_session_token
+        loop do 
+            session_token = SecureRandom::urlsafe_base64(16)
+            return session_token unless User.exists?(session_token: session_token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
 
 end
