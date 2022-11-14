f5fce05327ba1d6e6c15f60b998f657ec7cb33f5
Morgan Rector <mmrector@yahoo.com>
2022-11-13 18:02:06 -0800

    Rails auth, validations added to User Model

--- RailsAuth/app/models/user.rb
+++ RailsAuth/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    before_validation :ensure_session_token
+    validates :username, :session_token,  presence: true
+    validates :password_digest, presence: {message: "Password can't be blank"}
+    validates :password_digest, length: {minimum: 6, allow_nil: true}
+
+
+
 end
