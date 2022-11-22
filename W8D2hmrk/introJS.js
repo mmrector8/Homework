41a0106aa2977ecfb0a21e55da48054afc49bed2
Morgan Rector <mmrector@yahoo.com>
2022-11-21 18:43:30 -0800

    JavaScript first problems completed

---
+++ W8D2hmrk/introJS.js
@@ -0,0 +1,15 @@
+const madLib = function(verb, adj, noun) {
+    debugger
+    console.log(`We shall ${verb} the ${adj} ${noun}`);
+}
+
+madLib('run', 'quickly', 'rabbit')
+
+const isSubstring = function(searchString, subString) {
+    if (searchString.includes(subString)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
