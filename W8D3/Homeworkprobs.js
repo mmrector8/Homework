bd1e14ffb8f0bd99c9ee82438292758e43f5d34f
Morgan Rector <mmrector@yahoo.com>
2022-11-22 21:53:26 -0800

    Lela the elephant

--- W8D3/Homeworkprobs.js
+++ W8D3/Homeworkprobs.js
@@ -9,4 +9,29 @@ const callback = (array) => {
 }
 
 //console.log(titleize(['Morgan', 'Rector'], callback))
+//Constructors, Prototypes and This
+
+function Elephant(name, height, tricks){
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function (){
+    console.log(`${this.name} goes phrRRRRRRRRR!!!!!!!`)
+} 
+Elephant.prototype.grow = function(){
+    this.height += 12;
+}
+
+Elephant.prototype.addTrick = function(trick) {
+    this.tricks.push(trick);
+}
+
+Elephant.prototype.play = function(){
+    const trick = this.tricks[Math.floor(Math.random()*this.tricks.length)];
+    return trick;
+}
+
+const lela = new Elephant('Lela', 24, ['jump', 'spin', 'take carbon out of atmosphere'])
 
