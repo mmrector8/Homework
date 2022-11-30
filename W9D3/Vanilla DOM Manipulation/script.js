0ce2c9f6f9f7759c5a05e37864a3dacddc20af8f
Morgan Rector <mmrector@yahoo.com>
2022-11-29 21:40:40 -0800

    Toggle form added

--- W9D3/Vanilla DOM Manipulation/script.js
+++ W9D3/Vanilla DOM Manipulation/script.js
@@ -36,6 +36,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // --- your code here!
 
-
-
-});
+  const showPhotoForm = (e) => {
+    const photoFormCont = document.querySelector(".photo-form-container");
+    if (photoFormCont.className === "photo-form-container"){
+      photoFormCont.className = "photo-form-container hidden"
+    }else{
+      photoFormCont.className = "photo-form-container"
+    }
+  }
+  const photoFormShowButton = document.querySelector(".photo-show-button");
+  photoFormShowButton.addEventListener("click", showPhotoForm);
+});  
+  
+   
+  //const handlePhotoSubmit = (e) => { 
+ // e.preventDefault(); const photoUrlInput = document.querySelector(".photo-url-input"); const photoUrl = photoUrlInput.value; photoUrlInput.value = ""; const newImg = document.createElement("img"); newImg.src = photoUrl; const newPhotoLi = document.createElement("li"); newPhotoLi.appendChild(newImg); const dogPhotosList = document.querySelector(".dog-photos"); dogPhotosList.appendChild(newPhotoLi); }; const photoSubmitButton = document.querySelector(".photo-url-submit"); photoSubmitButton.addEventListener("click", handlePhotoSubmit);
