517bf31e9b96e8be01b1b082caa1aa2479eefd95
Morgan Rector <mmrector@yahoo.com>
2022-11-30 22:24:57 -0800

    AJAX request to London weather complete

--- W9D4/practice-for-ch-js-first-ajax/index.js
+++ W9D4/practice-for-ch-js-first-ajax/index.js
@@ -1,3 +1,19 @@
-console.log("Hello from the console!");
-
 // Your code here
+fetch("https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bcb83c4b54aee8418983c2aff3073b3b")
+.then(response => {
+    if(response.ok){
+        return response.json();
+    }else {
+        throw new Error(response);
+    }
+})
+.then(data => {
+    console.log("Success!");
+    console.log(data);
+})
+.catch(errorResponse => {
+    console.log("failure!");
+    console.error(errorResponse);
+});
+
+console.log("The AJAX request has been dispatched")
